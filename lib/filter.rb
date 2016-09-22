4e89332ebc38c68389d472c354cef8311f2a6dfc
Anna <anna.hollandsmith3@gmail.com>
2016-09-23 00:40:42 +0100

    changes to comments in code

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,18 +1,5 @@
-# def filter(word)
-#   if word !~ /covered|coloured|evergreen|blues|greenbelt/i
-#     word.gsub!(/red/i, 'r-d').gsub(/blue/i, 'bl--').gsub(/green/i, 'gr--n').gsub(/yellow/i, 'y-ll-w')
-#     if word =~ /covered|coloured|evergreen|blues|greenbelt/i
-#       word.gsub!(/red/i, 'r-d').gsub(/blue/i, 'bl--').gsub(/green/i, 'gr--n').gsub(/yellow/i, 'y-ll-w')
-#   else
-#     word
-#   end
-# end
-
-
-#An alternative syntax for the same method would be:
 
 def filter(word)
-  # if word !~ /covered|coloured|evergreen|blues|greenbelt/i
     word.gsub(/red|greenbelt|green|blues|blue|yellow|coloured|evergreen|covered|greenbelt/i,
                                                               'red' => 'r-d',
                                                               'green' => 'gr--n',
@@ -24,7 +11,15 @@ def filter(word)
                                                               'covered' => 'covered',
                                                               'greenbelt' => 'greenbelt')
 
-  # else
-  #   word
-  # end
 end
+
+#Original syntax prior to edgecase was:
+# def filter(word)
+#   if word !~ /covered|coloured|evergreen|blues|greenbelt/i
+#     word.gsub!(/red/i, 'r-d').gsub(/blue/i, 'bl--').gsub(/green/i, 'gr--n').gsub(/yellow/i, 'y-ll-w')
+#     if word =~ /covered|coloured|evergreen|blues|greenbelt/i
+#       word.gsub!(/red/i, 'r-d').gsub(/blue/i, 'bl--').gsub(/green/i, 'gr--n').gsub(/yellow/i, 'y-ll-w')
+#   else
+#     word
+#   end
+# end
